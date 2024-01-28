4daae1ad2a72b8c930c989e85cd25760de8f1dca
DariaZharkova <139045140+DariaZharkova@users.noreply.github.com>
2024-01-28 21:05:23 +0200

    radio

--- src/components/Radio/Radio.jsx
+++ src/components/Radio/Radio.jsx
@@ -6,36 +6,41 @@ export const Radio = ({ coffeeSize, setCoffeeSize }) => {
   return (
     <>
       <h1>Select coffee size</h1>
-      <label>
-        <input
-          type="radio"
-          name="coffeeSize"
-          value="sm"
-          checked={coffeeSize === 'sm'}
-          onChange={handleSizeChange}
-        />
-        Small
-      </label>
-      <label>
-        <input
-          type="radio"
-          name="coffeeSize"
-          value="md"
-          checked={coffeeSize === 'md'}
-          onChange={handleSizeChange}
-        />
-        Meduim
-      </label>
-      <label>
-        <input
-          type="radio"
-          name="coffeeSize"
-          value="lg"
-          checked={coffeeSize === 'lg'}
-          onChange={handleSizeChange}
-        />
-        Large
-      </label>
+      <div>
+        <label>
+          <input
+            type="radio"
+            name="coffeeSize"
+            value="sm"
+            checked={coffeeSize === 'sm'}
+            onChange={handleSizeChange}
+          />
+          Small
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="coffeeSize"
+            value="md"
+            checked={coffeeSize === 'md'}
+            onChange={handleSizeChange}
+          />
+          Meduim
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="coffeeSize"
+            value="lg"
+            checked={coffeeSize === 'lg'}
+            onChange={handleSizeChange}
+          />
+          Large
+        </label>
+      </div>
+      <p>
+        <b>Selected size:</b> {coffeeSize}
+      </p>
     </>
   );
 };
